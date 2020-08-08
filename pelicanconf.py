42350e49c893c8d8054371bc06e200299c248fc9
kolazojar <mkrolbass@gmail.com>
2020-08-08 01:12:34 -0400

    Added site subtitle and email to links.

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,8 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Jeff Krol'
-SITENAME = 'Jeff Krol Music'
+SITENAME = 'Jeff Krol'
+SITESUBTITLE = 'Drummer, Composer, Educator'
 SITEURL = ''
 
 PATH = 'content'
@@ -27,10 +28,10 @@ LINKS = (('Instagram', 'https://www.instagram.com/kroljeff'),
           ('SoundCloud', 'https://soundcloud.com/jeff-krol'),
           ('YouTube', 'https://www.youtube.com/channel/UCYCiON1AXbhwaOrzCYhD29g'),
           ('Facebook', 'https://www.facebook.com/pages/category/Musician-Band/Jeff-Krol-Music-802957153384805/'),
-          ('Matt Krol', 'https://mattkrol.me'))
+          ('E-mail', 'mailto:kroljeff@gmail.com'))
 
 # Social widget
-# SOCIAL = (('Facebook', '#'),)
+#SOCIAL = (('Social', '#'),)
 
 DEFAULT_PAGINATION = 10
 
